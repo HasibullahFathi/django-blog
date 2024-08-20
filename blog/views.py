a2b3a5d59f156efa91d4dd34e0633a0c881dd65a
Hasibullah Fathi <hasibullah.fathi@gmail.com>
2024-08-20 19:14:49 +0000

    create post detail page and add image to it

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -9,3 +9,27 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1) # filter is posible because of Post.objects instead of all() we can use e.g filter(author=4)
     template_name = "blog/index.html"  # now django pretends that our template name is post_list.html but if we have another name then we need this line of code
     paginate_by = 6
+
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
