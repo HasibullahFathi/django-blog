ab4cb6217722bca523b00d79ab69bb3b9d6b3190
Hasibullah Fathi <hasibullah.fathi@gmail.com>
2024-08-20 12:04:15 +0000

    add urls to the app level

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,6 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    # model = Post # it shows all the records of Post model but we cant not filter them with this statement
+    queryset = Post.objects.filter(status=0) # filter is posible because of Post.objects instead of all() we can use e.g filter(author=4)
+    template_name = "post_list.html"  # now django pretends that our template name is post_list.html but if we have another name then we need this line of code
